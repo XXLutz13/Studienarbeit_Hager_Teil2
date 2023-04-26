03a36bd9d41d6e3cdc8274a3ea506e49e43f70d8
XXLutz13 <lutz.hager@online.de>
2023-04-26 22:53:29 +0200

    Revert "testing without sleep"
    This reverts commit 692a7137df775d193e4c08ea4ef627c7983525db.

--- flask_app.py
+++ flask_app.py
@@ -78,10 +78,9 @@ def running():
     if not routine_active:
     # start backend routine
         routine_active = True
-        # routine = threading.Thread(target=start_routine)
-        # routine.daemon = True
-        # routine.start()
-        routine = start_routine()
+        routine = threading.Thread(target=start_routine)
+        routine.daemon = True
+        routine.start()
 
     global finished
     if finished:
@@ -183,7 +182,7 @@ def start_routine():
                 # wait for robot to set I91
                 while not ready:
                     ready = backend.read_value(I91_access)  # read I91
-                    # time.sleep(0.1)
+                    time.sleep(0.1)
 
                 # capturing image
                 try:
