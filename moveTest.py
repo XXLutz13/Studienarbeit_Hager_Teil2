8e74c826f8ad4fd7817a2d94e069834a06e4f26b
XXLutz13 <lutz.hager@online.de>
2023-05-25 21:17:10 +0200

    Adds error handlig if inpout fiels are empty

--- moveTest.py
+++ moveTest.py
@@ -1,4 +1,32 @@
 import pybcapclient.bcapclient as bcapclient    # Denso library for Cobotta access
+import numpy as np
+
+def coordinates(num_images, center):
+    R = 80
+    spacing = num_images//8
+
+    # phi = np.linspace(0, 0.5 * np.pi, spacing)
+    phi = np.linspace(0.85*np.pi, 0.5*np.pi, spacing)
+    X = [center[0]]*spacing
+    Y = center[1] - R * np.cos(phi)
+    Z = center[2] + R * np.sin(phi)
+    rx = np.linspace(1.15*90, 180, spacing)
+    ry = [0]*spacing
+    rz = [0]*spacing
+    num_steps = [50]*8
+    # type conversions
+    rx = rx.tolist()
+    Y = Y.tolist()
+    Z = Z.tolist()
+
+    # print(type(Y[0]))
+
+    cords = list(zip(X, Y, Z, rx, ry, rz))
+    # Round every element in the list of tuples using list comprehension and round()
+    cords = [(round(a, 4), round(b, 4), round(c, 4), round(d, 4), round(e, 4), round(f, 4)) for a,b,c,d,e,f in cords]
+    
+    return cords, num_steps
+
 
 # set IP Address , Port number and Timeout of connected RC8
 host = '192.168.0.1'
@@ -39,25 +67,43 @@ Param = [1, 0]
 m_bcapclient.robot_execute(HRobot, Command, Param)
 print("Motor On")
 
+# calculate arrays with roboter coordinates
+Objekt_cords = [190, -40, 110]
+cords, motorStepps = coordinates(40, Objekt_cords) 
+value = cords[0]
+# print(cords[0])
+
+# P90_handler = m_bcapclient.controller_getvariable(RC8, id, "P90")
+# m_bcapclient.variable_putvalue(P90_handler, value)
+
+# set ExtSpeed Speed,Accel,Decel
+Command = "ExtSpeed"
+Speed = 100
+Accel = 100
+Decel = 100
+Param = [Speed, Accel, Decel]
+m_bcapclient.robot_execute(HRobot, Command, Param)
+print("ExtSpeed")
 
 
-#---------------------------------------------------------------------
-#   moving robot 
-#---------------------------------------------------------------------
-# Move Initialize Position
-# Comp = 1
-# Pos_value = [0.0, 0.0, 90.0, 0.0, 90.0, 0.0]
-# Pose = [Pos_value, "P", "@E"]
-# m_bcapclient.robot_move(HRobot, Comp, Pose, "")
-# print("Complete Move P,@E J(0.0, 0.0, 90.0, 0.0, 90.0, 0.0)")
+try:
+    #---------------------------------------------------------------------
+    #   moving robot 
+    #---------------------------------------------------------------------
+    # Move Initialize Position
+    # Comp = 1
+    # Pos_value = [0.0, 0.0, 90.0, 0.0, 90.0, 0.0]
+    # Pose = [Pos_value, "P", "@E"]
+    # m_bcapclient.robot_move(HRobot, Comp, Pose, "")
+    # print("Complete Move P,@E J(0.0, 0.0, 90.0, 0.0, 90.0, 0.0)")
 
-# PoseData (array [Index , Variavletype , Pass])
-Comp = 1
-Pose = [2, "P", "@90"]
-m_bcapclient.robot_move(HRobot, Comp, Pose, "")
-print("Complete Move P90")
-
-
+    # Interpolation
+    # Comp = 1
+    # Pose = "P90"
+    m_bcapclient.robot_move(HRobot, 1, "P90", "NEXT")
+    print("Complete Move P90")
+except:
+    print("Failed to move robot")
 
 #---------------------------------------------------------------------
 #   closing robot connection
@@ -85,3 +131,5 @@ if(RC8 != 0):
 # End If
 m_bcapclient.service_stop()
 print("B-CAP service Stop")
+
+
